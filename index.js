ef9d9253cdc901fca4c694bec16d95bfe7cf36d6
smx <srxboys@126.com>
2020-07-26 17:19:13 +0800

    v0.0.2 [add] babel-cli

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 
-import UdeskJSApi from './services/udesk-js-api';
-
-import queryWithParams from './utils/query-with-params';
-import {sha1} from './utils/encryption/sha1'
-
-import getSignature from './src/signature';
-import UdeskJS from './src/udesk'
-
-let RXUdeskJS = UdeskJS;
-let RXUdeskJSApi = UdeskJSApi;
-
-let RXGetSignature = getSignature;
-let RXQueryWithParams = queryWithParams;
-let RXSha1 = sha1;
+import {
+  RXUdeskJS,
+  RXUdeskJSApi,
+  RXSha1,
+  RXGetSignature,
+  RXQueryWithParams,
+} from './lib'
 
 export default RXUdeskJS;
 
